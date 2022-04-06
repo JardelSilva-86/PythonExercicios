85db0b8fef00a57db945a3b97008b32199e8feaa
Jardel Silva <jardel.s22@gmail.com>
2022-04-05 21:38:15 -0300

    Mudei o exercício

---
+++ Mundo1/Exercicio1.py
@@ -0,0 +1,5 @@
+### Crie um programa que leia o nome da pessoa e mostre uma msg de boas vindas.
+
+print('=' * 7, 'DESAFIO 1', '=' * 7)
+nome = input('Qual é o seu nome? ')
+print('Olá, {}! É um prazer te conhecer!'.format(nome))

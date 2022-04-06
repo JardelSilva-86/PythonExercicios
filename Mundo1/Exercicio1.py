6fc7196246baaf31b848fcc321e763bad88f0e63
Jardel Silva <jardel.s22@gmail.com>
2022-04-05 21:59:08 -0300

    Update Exercicio1.py

--- Mundo1/Exercicio1.py
+++ Mundo1/Exercicio1.py
@@ -1,7 +1,6 @@
 ### Crie um programa que leia o nome da pessoa e mostre uma msg de boas vindas.
 
-print('=-' *7, 'DESAFIO 2', '=-'*7)
-dia = int(input('Qual o dia de seu nascimento? '))
-mes = int(input('Qual o mês do seu nascimento? '))
-ano = int(input('Qual é o ano do seu nascimento? '))
-print('Você nasceu no dia {:2} de {} de {}, correto?'.format(dia, mes, ano))
+# Crie um programa que leia o nome da pessoa e mostre uma msg de boas vindas.
+print('===== DESAFIO 1 =====')
+nome = input('Qual é o seu nome? ')
+print('Olá ,{}! É um prazer te conhecer!'.format(nome))

3283b626b9817621bb4fd3099bd37c0b8eb80bed
Jardel Silva <jardel.s22@gmail.com>
2022-04-05 21:56:50 -0300

    Exercício 2

---
+++ Mundo1/Exercicio2.py
@@ -0,0 +1,7 @@
+# Crie um programa que solicite a data de nascimento de uma pessoa.
+
+print('=-' *7, 'DESAFIO 2', '=-'*7)
+dia = int(input('Qual o dia de seu nascimento? '))
+mes = str(input('Qual o mês do seu nascimento? '))
+ano = int(input('Qual é o ano do seu nascimento? '))
+print('Você nasceu no dia {} de {} de {}, correto?'.format(dia, mes, ano))

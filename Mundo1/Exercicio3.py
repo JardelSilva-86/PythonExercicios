335eebba0b2cdb8f4bd847d13388cc4a5782b9b1
Jardel Silva <jardel.s22@gmail.com>
2022-04-06 07:27:45 -0300

    Create Exercicio3.py

---
+++ Mundo1/Exercicio3.py
@@ -0,0 +1,7 @@
+# Crie um program que some dois números
+
+print('=-' *7, 'DESAFIO 3', '=-'*7)
+priNum = int(input('Digite o primeiro número! '))
+segNum = int(input('Digite o segundo número! '))
+soma = priNum + segNum
+print('A soma entre {} e {} vale {}.'.format(priNum, segNum, soma))

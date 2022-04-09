5a9231940292b9b391463a6dead75587c5be0217
Jardel Silva <jardel.s22@gmail.com>
2022-04-09 06:37:44 -0300

    Create Exercicio7.py

---
+++ Mundo1/Exercicio7.py
@@ -0,0 +1,5 @@
+# Crie um programa que peça um número ao usuário e retorne se antecessor e o seu sucessor.
+
+print('=-'*7 'DESAFIO 7' ''=-'*7)
+n = int(input('Digite um número: '))
+print('Analisando o número {}, seu antecessor é {} e seu sucessor é {}  '.format(n, n - 1, n + 2))

eb42c8f5582fa59bf968b5f031090c8b1806b56f
Jardel Silva <jardel.s22@gmail.com>
2022-04-12 21:16:55 -0300

    Update Exercicio16

---
+++ Mundo1/Exercicio16.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteiro
+
+from math import trunc
+n = float( input( 'Digite um número decimal: ') )
+print( trunc( n ) )
--- Mundo1/Exercício9.py
+++
@@ -1,6 +0,0 @@
-# Crie um progrma que converta a temperatura digitada em ºC para a temperatura em ºF
-
-print('=-'*7, 'DESAFIO 9', '=-'*7)
-cel = float(input("Digite a temperatura em ºC: "))
-fahre = ((9 * cel) / 5) + 32
-print("A temperatura de {}ºC corresponde a {}ºF".format(cel,fahre))

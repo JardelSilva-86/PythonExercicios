38d0bc4c68a84417b069dea6304bf574173658d0
Jardel Silva <jardel.s22@gmail.com>
2022-04-12 21:41:28 -0300

    Update Exercicio16.py

--- Mundo1/Exercicio16.py
+++ Mundo1/Exercicio16.py
@@ -1,5 +1,6 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteiro
 
+print( '=-'*7, 'DESAFIO 16', '=-'*7 )
 from math import trunc
 n = float( input( 'Digite um número decimal: ') )
 print( trunc( n ) )
